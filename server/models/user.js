329d90f9f412827745045e0cecdb6639121b459e
xieanne <anne.xie@mail.utoronto.ca>
2021-03-31 21:02:46 -0400

    added body parser

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,67 @@
-const mongoose = require('mongoose');
+'use strict'
 
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+// Making a Mongoose model a little differently: a Mongoose Schema
+// Allows us to add additional functionality.
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-});
+	username: {
+		type: String,
+		required: true,
+		minlength: 1,
+		unique: true,
+	}, 
+	password: {
+		type: String,
+		required: true,
+		minlength: 8
+	}
+})
+
+// An example of Mongoose middleware.
+// This function will run immediately prior to saving the document
+// in the database.
+UserSchema.pre('save', function(next) {
+	const user = this; // binds this to User document instance
+
+	// checks to ensure we don't hash password more than once
+	if (user.isModified('password')) {
+		// generate salt and hash the password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
+	}
+})
+
+// A static method on the document model.
+// Allows us to find a User document by comparing the hashed password
+//  to a given one, for example when logging in.
+UserSchema.statics.findByUserNamePassword = function(username, password) {
+	const User = this // binds this to the User model
+
+	// First find the user by their email
+	return User.findOne({ username: username }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
 
 const User = mongoose.model('User', UserSchema);
 
