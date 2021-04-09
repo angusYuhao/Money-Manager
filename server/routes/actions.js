b75426d99277b8a8a0f2a3c61d16b4744533e9de
unknown <ianc99988@gmail.com>
2021-04-09 00:10:53 -0700

    Separated community and user routes into their own files

---
+++ server/routes/actions.js
@@ -0,0 +1,15 @@
+// middleware for mongo connection error for routes that need it
+const mongoChecker = (req, res, next) => {
+    // check mongoose connection established.
+    if (mongoose.connection.readyState != 1) {
+        log('Issue with mongoose connection')
+        res.status(500).send('Internal server error')
+        return;
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    mongoChecker: mongoChecker
+}
